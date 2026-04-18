d12fe68423352c0ff099e4d94d889ac7fef6448b
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-04-18 15:30:28 +0200

    Zadanie 1 - nowe encje w modelu

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,28 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String country;
+    private String city;
 }
+
